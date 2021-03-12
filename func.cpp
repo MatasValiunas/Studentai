9df83056235f8bd7c66ddab2a6ae5ee002dc11f0
Matas Valiūnas <72632243+MatasValiunas@users.noreply.github.com>
2021-03-12 14:45:32 +0200

    Update func.cpp

--- func.cpp
+++ func.cpp
@@ -45,7 +45,6 @@ void IvedimasRanka(vector<Stud>& A){
 }
 
 bool IvedimasIsFailo(vector<Stud>& A, double Laik[]){
-    auto start = high_resolution_clock::now();
 
     int failas;
     cout << "Failo skaicius, is kurio bus atliktas skaitymas [Studentai ... .txt]: ";
@@ -54,6 +53,8 @@ bool IvedimasIsFailo(vector<Stud>& A, double Laik[]){
         cin.clear();
 		cin.ignore(128, '\n');
     }
+
+    auto start = system_clock::now();
 
     try {
         ifstream in("Studentai" + to_string(failas) + ".txt");
@@ -81,7 +82,7 @@ bool IvedimasIsFailo(vector<Stud>& A, double Laik[]){
         return false;
     }
 
-    auto end  = high_resolution_clock::now();
+    auto end  = system_clock::now();
     duration<double> diff = end - start;
     Laik[1] = diff.count();
 
@@ -104,54 +105,57 @@ double Mediana(vector<int>& paz){
         return ((double)paz[n/2 - 1] + paz[n/2]) / 2;
 }
 
-void Isvedimas(vector<Stud> A, double Laik[]){
-    auto start = high_resolution_clock::now();
-
-    ofstream out("Rezultatai.txt");
+void Isvedimas(vector<Stud>& A, double Laik[]){
     char VidMed;
     cout << "Rodyti vidurki ar mediana? [V/M]: ";
     cin >> VidMed;
-    sort(A.begin(), A.end());
-
     while (toupper(VidMed) != 'V' && toupper(VidMed) != 'M'){
         cout << "Neteisinga ivestis!" << endl << "Rodyti vidurki ar mediana? [V/M]: ";
         cin >> VidMed;
     }
+
+    sort(A.begin(), A.end());
+    vector<Stud> Kiet;
+    vector<Stud> Minkst;
+
+    auto start = system_clock::now();               // Rusiavimas i 2 grupes
+    Rusiavimas(A, Kiet, Minkst, VidMed, Laik);
+    auto end = system_clock::now();
+    duration<double> diff = end - start;
+    Laik[2] = diff.count();
+
+    start = system_clock::now();                   // Isvedimas
+
     ofstream outK("RezultataiKieti.txt");
     ofstream outM("RezultataiMinksti.txt");
     if (toupper(VidMed) == 'V'){
         outK << "Pavarde                  Vardas             Galutinis (Vid.)" << endl;
-        outK << "------------------------------------------------------------" << endl;
         outM << "Pavarde                  Vardas             Galutinis (Vid.)" << endl;
-        outM << "------------------------------------------------------------" << endl;
-        for (int i=0; i<A.size(); i++){
-            if (round(0.4 * Vidurkis(A[i].nd) + 0.6 * A[i].egz) >= 5)
-                outK <<fixed<<left<<setw(25)<< A[i].vard <<setw(25)<< A[i].pav <<setprecision(0)<< round(0.4 * Vidurkis(A[i].nd) + 0.6 * A[i].egz) << endl;
-            else
-                outM <<fixed<<left<<setw(25)<< A[i].vard <<setw(25)<< A[i].pav <<setprecision(0)<< round(0.4 * Vidurkis(A[i].nd) + 0.6 * A[i].egz) << endl;
-        }
     }
     else {
         outK << "Pavarde                  Vardas             Galutinis (Med.)" << endl;
-        outK << "------------------------------------------------------------" << endl;
         outM << "Pavarde                  Vardas             Galutinis (Med.)" << endl;
-        outM << "------------------------------------------------------------" << endl;
-        for (int i=0; i<A.size(); i++){
-            if (round(0.4 * Mediana(A[i].nd) + 0.6 * A[i].egz) >= 5)
-                outK <<fixed<<left<<setw(25)<< A[i].vard <<setw(25)<< A[i].pav <<setprecision(0)<< round(0.4 * Mediana(A[i].nd) + 0.6 * A[i].egz) << endl;
-            else
-                outM <<fixed<<left<<setw(25)<< A[i].vard <<setw(25)<< A[i].pav <<setprecision(0)<< round(0.4 * Mediana(A[i].nd) + 0.6 * A[i].egz) << endl;
-        }
     }
-    out.close();
+    outK << "------------------------------------------------------------" << endl;
+    outM << "------------------------------------------------------------" << endl;
+    for (int i=0; i<Kiet.size(); i++)
+        outK <<fixed<<left<<setw(25)<< Kiet[i].vard <<setw(25)<< Kiet[i].pav << Kiet[i].galut << endl;
 
-    auto end = high_resolution_clock::now();
-    duration<double> diff = end - start;
-    Laik[2] = diff.count();
+    for (int i=0; i<Minkst.size(); i++)
+        outM <<fixed<<left<<setw(25)<< Minkst[i].vard <<setw(25)<< Minkst[i].pav << Minkst[i].galut << endl;
+    outK.close();
+    outM.close();
 
-    cout << endl << "Duomenu kurimo laikas: " << Laik[0] << " s" << endl;
-    cout << "Duomenu skaitymo laikas: " << Laik[1] << " s" << endl;
-    cout << "Studentu rusiavimo ir isvedimo laikas: " << Laik[2] << " s" << endl;
+    end = system_clock::now();
+    diff = end - start;
+    Laik[3] = diff.count();
+
+    if (Laik[0] != 0)
+        cout << endl << "Duomenu generavimo ir irasymo i faila laikas: " << Laik[0] << " s" << endl;
+    if (Laik[1] != 0)
+        cout << "Duomenu skaitymo laikas: " << Laik[1] << " s" << endl;
+    cout << "Studentu rusiavimo laikas: " << Laik[2] << " s" << endl;
+    cout << "Isvedimo i failus laikas: " << Laik[3] << " s" << endl;
 }
 
 bool IvestisYN(string tekstas){
@@ -164,8 +168,7 @@ bool IvestisYN(string tekstas){
     }
     if (toupper(naujas) == 'Y')
         return true;
-    else
-        return false;
+    return false;
 }
 
 int IvestisSk(string tekstas, bool check0to10){
@@ -186,12 +189,10 @@ void RandomPridejimas(vector<int>& nd){
 
 void Generavimas(double Laik[]){
     int stud, nd;
-    cout << "Studentu skaicius: ";
-    cin >> stud;
-    cout << "Namu darbu skaicius: ";
-    cin >> nd;
+    stud = IvestisSk("Studentu skaicius: ", false);
+    nd = IvestisSk("Namu darbu skaicius: ", false);
 
-    auto start = high_resolution_clock::now();
+    auto start = system_clock::now();
 
     ofstream out("Studentai" + to_string(stud) + ".txt");
     out <<fixed<<setw(25)<<left<< "Vardas" <<setw(25)<< "Pavarde";
@@ -199,20 +200,36 @@ void Generavimas(double Laik[]){
         out <<fixed<<setw(10)<<left<< "ND" + to_string(i);
     out << "Egz." << endl;
 
-    for (int i=1; i<=stud; i++){
+    for (int i=1; i<stud; i++){
         out <<fixed<<setw(25)<<left<< "Vardas" + to_string(i) <<setw(25)<< "Pavarde" + to_string(i);
         for (int i=0; i<nd; i++)
             out <<fixed<<setw(10)<<left<< dist(mt);
-        out << dist(mt);
-        if (i != stud)
-            out << endl;
+        out << dist(mt) << endl;
     }
+    out <<fixed<<setw(25)<<left<< "Vardas" + to_string(stud) <<setw(25)<< "Pavarde" + to_string(stud);
+    for (int i=0; i<nd; i++)
+        out <<fixed<<setw(10)<<left<< dist(mt);
+    out << dist(mt);
     out.close();
 
-    auto end = high_resolution_clock::now();
+    auto end = system_clock::now();
     duration<double> diff = end - start;
     Laik[0] = diff.count();
 
     if (!IvestisYN("toliau testi programa"))
         exit(1);
+}
+
+void Rusiavimas(vector<Stud>& A, vector<Stud>& Kiet, vector<Stud>& Minkst, char VidMed, double Laik[]){       
+    double (*func)(vector<int>&){&Vidurkis};
+    if (toupper(VidMed) == 'M')
+        func = &Mediana;
+
+    for (int i=0; i<A.size(); i++){
+        A[i].galut = round(0.4 * func(A[i].nd) + 0.6 * A[i].egz);
+        if (A[i].galut < 5)
+            Minkst.push_back(A[i]);
+        else
+            Kiet.push_back(A[i]);
+    }
 }
